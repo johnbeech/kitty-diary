56a92475d4e90b63b87997860a66d84a15779100
John Beech <johnbeech.github@mkv25.net>
2019-03-03 17:46:58 +0000

    Add script to download feeding diary from google sheets

---
+++ scripts/download-feeding-diary.js
@@ -0,0 +1,24 @@
+const gsjson = require('google-spreadsheet-to-json')
+const { write, position } = require('promise-path')
+const datapath = position(__dirname, '../data')
+const spreadsheetId = '1-PRw2Ve-1iTNhXcxadSQ7mgZWWD513UbjjVKh8AkwAg'
+const report = (...messages) => console.log('[Download Feeding Diary]', ...messages)
+
+async function downloadData (spreadsheetId) {
+  try {
+    const worksheets = await gsjson({ spreadsheetId, allWorksheets: true })
+    report('Downloaded data:', (worksheets + '').length, 'bytes')
+    const entries = worksheets.reduce((acc, item) => acc.concat(item), [])
+    const filename = 'feeding-diary.json'
+    report('Writing combined data to:', filename)
+    write(datapath(filename), JSON.stringify(entries, null, 2), 'utf8')
+  } catch (ex) {
+    report('Unable to download data:', ex)
+  }
+}
+
+async function start () {
+  return downloadData(spreadsheetId)
+}
+
+module.exports = start
