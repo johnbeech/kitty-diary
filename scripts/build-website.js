59db1f253e2110d49ce0da68a4f4ae6203333921
John Beech <johnbeech@users.noreply.github.com>
2019-03-04 21:59:39 +0000

    Build template (#1)
    * Build-website script using handlebars
    
    * Only target template files; ignore non css, html, js files
    
    * Add build step to copy static files
    
    * Add favicon to site

---
+++ scripts/build-website.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const { read, write, position, make, find } = require('promise-path')
+const handlebars = require('handlebars')
+
+const datapath = position(__dirname, '../data')
+const templatepath = position(__dirname, '../templates')
+const buildpath = position(__dirname, '../build')
+const report = (...messages) => console.log('[Build Website]', ...messages)
+
+async function processTemplates (files, data) {
+  const templateWork = files.map(filePath => processTemplate(filePath, data))
+  return Promise.all(templateWork)
+}
+
+async function processTemplate(filePath, data) {
+  const templateString = await read(filePath, 'utf8')
+  const template = handlebars.compile(templateString)
+  const outputString = template(data)
+
+  const templatePathBase = templatepath('./')
+  const fragmentPath = filePath.substring(templatePathBase.length)
+  const fragmentBasePath = path.dirname(fragmentPath)
+  const outFilePath = buildpath(fragmentPath)
+
+  report('Writing template:', fragmentPath, outputString.length, 'bytes')
+  await make(fragmentBasePath)
+  return write(outFilePath, outputString, 'utf8')
+}
+
+async function copyStaticFiles(files) {
+  const staticFileWork = files.map(copyStaticFile)
+  return Promise.all(staticFileWork)
+}
+
+async function copyStaticFile(filePath) {
+  const staticFile = await read(filePath)
+  const fileBasePath = templatepath('./')
+  const fragmentPath = filePath.substring(fileBasePath.length)
+  const fragmentBasePath = path.dirname(fragmentPath)
+  const outFilePath = buildpath(fragmentPath)
+
+  report('Copying file:', fragmentPath, staticFile.length, 'bytes')
+  await make(fragmentBasePath)
+  return write(outFilePath, staticFile)
+}
+
+async function findTemplateFiles() {
+  const files = await find(templatepath('**/*.*'))
+  return files.filter(n => /(css|html|js)/.test(n))
+}
+
+async function findStaticFiles() {
+  const files = await find(templatepath('**/*.*'))
+  return files.filter(n => !/(css|html|js)/.test(n))
+}
+
+async function start () {
+  const templateFiles = await findTemplateFiles()
+  const staticFiles = await findStaticFiles()
+  const data = {}
+  await processTemplates(templateFiles, data)
+  await copyStaticFiles(staticFiles)
+  return true
+}
+
+module.exports = start
