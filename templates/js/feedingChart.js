b4f8f2d59723244495efb8da5e8bdd40e8709998
John Beech <johnbeech.github@mkv25.net>
2019-03-30 12:25:38 +0000

    Add donut chart with static test data

---
+++ templates/js/feedingChart.js
@@ -0,0 +1,38 @@
+const feedingCharts = []
+
+function insertFeedingChart() {
+  const chartId = `feedingChart-${feedingCharts.length}`
+  document.write(`<canvas id="${chartId}" width="100%" height="50vh"></canvas>`)
+  const ctx = document.getElementById(chartId).getContext('2d')
+  const chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.9)',
+          'rgba(54, 162, 235, 0.9)',
+          'rgba(255, 206, 86, 0.9)',
+          'rgba(75, 192, 192, 0.9)',
+          'rgba(153, 102, 255, 0.9)',
+          'rgba(255, 159, 64, 0.9)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 3
+      }]
+    },
+    options: {}
+  })
+  feedingCharts.push(chart)
+}
+
+document.insertFeedingChart = insertFeedingChart
